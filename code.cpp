436792a3b43cf92411f49bc2b47c21a420feda8f
Hammad Rashid (Hammad) <76726810+Hammad1007@users.noreply.github.com>
2022-02-16 20:35:55 +0500

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -3,3 +3,30 @@
 #include <unistd.h>
 #include <string.h>
 #include <cstring>
+
+using namespace std;
+
+int main() {
+  string s;
+  cout << "Enter the string: ";
+  getline(cin, s);
+  cout << "The original string entered is: " << s << "\n";
+  int size = s.length();
+  pid_t pid;  
+  pid = fork();
+  if(pid == 0) {
+    // means child created
+    for(int i = 0; i < size; i++) {
+       s[i] = s[i] + 5;  // the encryption is set to 5 
+    }
+    cout << "The encrypted string is: " << s << "\n";
+    
+    for(int i = 0; i < size; i++) {
+       s[i] = s[i] - 5;  // the decryption is set to 5  
+    }
+    cout << "The decrypted message is: " << s << "\n";
+  }
+  else {
+    wait(NULL);
+  }
+}
