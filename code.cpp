28354ebfe57522f7cbb06d8e1e7ee44556e9902d
Hammad Rashid (Hammad) <76726810+Hammad1007@users.noreply.github.com>
2022-02-28 09:34:33 +0500

    updated code.cpp

--- code.cpp
+++ code.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 #include <sys/wait.h>>
 #include <unistd.h>
-#include <string.h>
+#include <string.h>  // handling the strings in c++
 #include <cstring>
 
 using namespace std;
 
+// Main driver starts here
 int main() {
   string s;
   cout << "Enter the string: ";
   getline(cin, s);
   cout << "The original string entered is: " << s << "\n";
-  int size = s.length();
+  int size = s.length();  // built in function to find the length of string
   pid_t pid;   // process id variable
   pid = fork();
+  // fork returs 0 for a child process and any non zero integer for parent process
   if(pid == 0) {
     // means child created
     for(int i = 0; i < size; i++) {
-       s[i] = s[i] + 5;  // the encryption is set to 5 
+       s[i] = s[i] + 5;   // the encryption is set to 5, eg: letter a is encrypted to e in the encrypted message
     }
     cout << "The encrypted string is: " << s << "\n";
     
