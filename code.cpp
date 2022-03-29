e7c38fef30e095a980d929fbf8b085120743e6a0
Hammad Rashid (Hammad) <76726810+Hammad1007@users.noreply.github.com>
2022-03-29 20:03:46 +0500

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -13,9 +13,12 @@ int main() {
   getline(cin, s);
   cout << "The original string entered is: " << s << "\n";
   int size = s.length();  // built in function to find the length of string
+  
   pid_t pid;   // process id variable
   pid = fork();
+  
   // fork returs 0 for a child process and any non zero integer for parent process
+  
   if(pid == 0) {
     // means child created
     for(int i = 0; i < size; i++) {
